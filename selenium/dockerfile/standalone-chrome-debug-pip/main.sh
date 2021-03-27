014c9d8bca00baffc56cc646a06a63d18ee2abec
Ruanun <ruanun@foxmail.com>
2021-03-27 15:01:12 +0800

    添加 standalone-chrome-debug

---
+++ selenium/dockerfile/standalone-chrome-debug-pip/main.sh
@@ -0,0 +1,3 @@
+bash /opt/bin/start-base.sh
+sudo python3 demo.py
+#docker run -it --rm --name scd -p 4444:4444 -p 5900:5900 -v /code:/code -w /code/ -v /dev/shm:/dev/shm ruanun/standalone-chrome-debug bash main.sh
--- selenium/dockerfile/standalone-chrome-debug-pip/start-base.sh
+++ selenium/dockerfile/standalone-chrome-debug-pip/start-base.sh
@@ -1,1 +1,1 @@
-nohup bash /opt/bin/entry_point.sh > /log/start.log 2>&1 &
+nohup bash /opt/bin/entry_point.sh > ~/start.log 2>&1 &
