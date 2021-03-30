7295ee9f3a4d543167300af38cc1fda8f8a53ce7
ruanun <ruanun@foxmail.com>
2021-03-30 16:28:23 +0800

    add ubuntu

--- selenium/dockerfile/ubuntu-xfce-vnc-selenium/build.sh
+++ selenium/dockerfile/ubuntu-xfce-vnc-selenium/build.sh
@@ -1,4 +1,6 @@
 docker build -t ruanun/ubuntu-xfce-vnc-selenium .
 
+#docker push ruanun/ubuntu-xfce-vnc-selenium
+
 #docker run -d -p 5901:5901 -p 6901:6901 --rm --user 0 -e VNC_PW=my-pw ruanun/ubuntu-xfce-vnc-selenium
 #docker run -d -p 5901:5901 -p 6901:6901 --rm  -e VNC_PW=my-pw ruanun/ubuntu-xfce-vnc-selenium
